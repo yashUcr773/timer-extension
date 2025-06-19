9ea96cd710f2e3478aea59d5943ec77e357ccce9
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-06-19 09:59:18 +0530

    react + shadcn setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <Button variant={'outline'}>Hello</Button>
+    </>
+  )
+}
+
+export default App
